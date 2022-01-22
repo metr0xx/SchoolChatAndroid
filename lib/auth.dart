3fff4f63117695a9141c727f3865e9044777d98e
metr0xx <mishgan0702@gmail.com>
2022-01-22 19:37:01 +0300

    upd

--- lib/auth.dart
+++ lib/auth.dart
@@ -13,139 +13,87 @@ class Auth extends StatelessWidget {
   Widget build(BuildContext context) {
     String _email;
     String _password;
-    Text auth = Text(
-      Strings.auth, 
-      textDirection: TextDirection.ltr, 
-      style: TextStyle(
-        fontSize: 35, 
-        color: Color(0xFF75d9c8), 
-        fontWeight: FontWeight.bold
-        )
-      );
+    Text auth = Text(Strings.auth,
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontSize: 35,
+            color: Color(0xFF75d9c8),
+            fontWeight: FontWeight.bold));
     Text bigclear = Text('', style: TextStyle(fontSize: 60));
     Text midclear = Text('', style: TextStyle(fontSize: 45));
 
     TextField login = TextField(
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        hintText: Strings.email,
-        hintStyle: TextStyle(color: Color(0xFFa40dd6)),
-        fillColor: Color(0xFFbfbfbf),
-        filled: true,
-      )
-    );
+        decoration: InputDecoration(
+      border: InputBorder.none,
+      hintText: Strings.email,
+      hintStyle: TextStyle(color: Color(0xFFa40dd6)),
+      fillColor: Color(0xFFbfbfbf),
+      filled: true,
+    ));
 
     TextField password = TextField(
       decoration: InputDecoration(
         border: InputBorder.none,
-        hintStyle: TextStyle(color: Color(0xFFa40dd6), ),
+        hintStyle: TextStyle(
+          color: Color(0xFFa40dd6),
+        ),
         hintText: Strings.password,
         fillColor: Color(0xFFbfbfbf),
         filled: true,
-    ),
-  );
+      ),
+    );
 
     ElevatedButton signIn = ElevatedButton(
-      onPressed: () {
-        start_connection();
-        Navigator.push(
-          context, 
-          MaterialPageRoute(
-            builder: (context) => Chats()
-          )
-        );
-        // showDialog(
-        //   context: context,     
-        //   builder: (context) {
-        //     Future.delayed(          
-        //       Duration(seconds: 3), () {     
-        //         // print('loaded in AUTH');
-        //         Navigator.push(
-        //           context, 
-        //           MaterialPageRoute(
-        //             builder: (context) => Chats()
-        //           )
-        //         );                                    
-        //       }
-        //     );                        
-        //     return AlertDialog(
-        //       title: Text('Загрузка чатов...'),
-        //     );
-        //   }
-        // );
-      },
-      child: Text(
-        Strings.signIn, 
-        style: TextStyle(
-          color: Color(0xFFa40dd6), 
-          fontSize: 25
-          )
-        ),
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(
-          Color(0xFF1c1a1c)
-          ), 
-        )
-      );
+        onPressed: () {
+          start_connection();
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Chats()));
+        },
+        child: Text(Strings.signIn,
+            style: TextStyle(color: Color(0xFFa40dd6), fontSize: 25)),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Color(0xFF1c1a1c)),
+        ));
 
     ElevatedButton signUp = ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context, 
-          MaterialPageRoute(
-            builder: (context) => SignUpPage()
-          )
-        );
-      },
-     child: Text(
-       Strings.signUp, 
-       style: TextStyle(
-         color: Color(0xFFa40dd6), 
-         fontSize: 25
-         ),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SignUpPage()));
+        },
+        child: Text(
+          Strings.signUp,
+          style: TextStyle(color: Color(0xFFa40dd6), fontSize: 25),
         ),
-     style: ButtonStyle(
-       backgroundColor: MaterialStateProperty.all(
-         Color(0xFF1c1a1c)
-        ),
-       )
-      );
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Color(0xFF1c1a1c)),
+        ));
 
     Row inup = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-      children: <Widget>[
-        signIn, signUp
-        ]
-      );
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[signIn, signUp]);
 
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            Strings.homeTittle, 
-            textScaleFactor: 1, 
-            style: TextStyle(
-              fontSize: 25
-              )
-            ), 
-            backgroundColor: Color(0xFF350752), 
-            centerTitle: false,
-          ),
-        body: Container(
-          color: Color(0xFF140513),
-          // color: Colors.white,
-          alignment: FractionalOffset(0.5, 0.2), 
-          child: Column(   
-            children: <Widget>[
-              bigclear, auth, bigclear, login, midclear, password, bigclear, Center(
-                child: inup
-                )
-              ]           //child: test,
-          )
-        )
-      )
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(Strings.homeTittle,
+                  textScaleFactor: 1, style: TextStyle(fontSize: 25)),
+              backgroundColor: Color(0xFF350752),
+              centerTitle: false,
+            ),
+            body: Container(
+                color: Color(0xFF140513),
+                // color: Colors.white,
+                alignment: FractionalOffset(0.5, 0.2),
+                child: Column(children: <Widget>[
+                  bigclear,
+                  auth,
+                  bigclear,
+                  login,
+                  midclear,
+                  password,
+                  bigclear,
+                  Center(child: inup)
+                ] //child: test,
+                    ))));
   }
 }
-
-
