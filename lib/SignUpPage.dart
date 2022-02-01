36281ddf17aa82a248478396c054aaba35653c7b
metr0xx <mishgan0702@gmail.com>
2022-02-01 22:07:01 +0300

    upd

--- lib/SignUpPage.dart
+++ lib/SignUpPage.dart
@@ -13,22 +13,40 @@ class _SignUpStageState extends State<SignUpPage> {
     Container registr = Container(
         padding: EdgeInsets.only(
             bottom: MediaQuery.of(context).size.height / 20,
-            top: MediaQuery.of(context).size.height / 18),
+            top: MediaQuery.of(context).size.height / 15),
         child: Text("Регистрация",
             textDirection: TextDirection.ltr,
             style: TextStyle(
-                fontSize: 35,
-                color: Colors.purple[800],
-                fontWeight: FontWeight.bold,
-                fontFamily: "OpenSans")));
+              fontSize: 35,
+              color: Colors.purple[800],
+              fontWeight: FontWeight.bold,
+              fontFamily: "Helvetica",
+              shadows: const <Shadow>[
+                Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 5.0,
+                    color: Colors.purple),
+              ],
+            )));
     Container surname = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 5,
+              blurRadius: 5,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
         width: MediaQuery.of(context).size.width / 1.2,
         height: MediaQuery.of(context).size.height / 12,
         child: TextField(
             decoration: InputDecoration(
                 hintText: "Фамилия",
                 hintStyle: TextStyle(
-                    color: Colors.purple[900], fontFamily: "OpenSans"),
+                    color: Colors.purple[900], fontFamily: "Helvetica"),
                 prefixIcon: Icon(
                   Icons.account_circle_outlined,
                   color: Colors.purple[900],
@@ -38,13 +56,24 @@ class _SignUpStageState extends State<SignUpPage> {
                 fillColor: Colors.white,
                 filled: true)));
     Container name = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
         width: MediaQuery.of(context).size.width / 1.2,
         height: MediaQuery.of(context).size.height / 11.5,
         child: TextField(
             decoration: InputDecoration(
                 hintText: "Имя",
                 hintStyle: TextStyle(
-                    color: Colors.purple[900], fontFamily: "OpenSans"),
+                    color: Colors.purple[900], fontFamily: "Helvetica"),
                 prefixIcon: Icon(
                   Icons.account_circle_outlined,
                   color: Colors.purple[900],
@@ -54,15 +83,26 @@ class _SignUpStageState extends State<SignUpPage> {
                 fillColor: Colors.white,
                 filled: true)));
     Container email = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
         width: MediaQuery.of(context).size.width / 1.2,
         height: MediaQuery.of(context).size.height / 11.5,
         child: TextField(
             decoration: InputDecoration(
                 hintText: "Эл. почта",
                 hintStyle: TextStyle(
-                    color: Colors.purple[900], fontFamily: "OpenSans"),
+                    color: Colors.purple[900], fontFamily: "Helvetica"),
                 prefixIcon: Icon(
-                  Icons.account_circle_outlined,
+                  Icons.email_outlined,
                   color: Colors.purple[900],
                 ),
                 border: OutlineInputBorder(
@@ -70,15 +110,26 @@ class _SignUpStageState extends State<SignUpPage> {
                 fillColor: Colors.white,
                 filled: true)));
     Container phone = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
         width: MediaQuery.of(context).size.width / 1.2,
         height: MediaQuery.of(context).size.height / 11.5,
         child: TextField(
             decoration: InputDecoration(
                 hintText: "Номер телефона",
                 hintStyle: TextStyle(
-                    color: Colors.purple[900], fontFamily: "OpenSans"),
+                    color: Colors.purple[900], fontFamily: "Helvetica"),
                 prefixIcon: Icon(
-                  Icons.account_circle_outlined,
+                  Icons.phone_outlined,
                   color: Colors.purple[900],
                 ),
                 border: OutlineInputBorder(
@@ -86,15 +137,26 @@ class _SignUpStageState extends State<SignUpPage> {
                 fillColor: Colors.white,
                 filled: true)));
     Container password = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
         width: MediaQuery.of(context).size.width / 1.2,
         height: MediaQuery.of(context).size.height / 11.5,
         child: TextField(
             decoration: InputDecoration(
                 hintText: "Пароль",
                 hintStyle: TextStyle(
-                    color: Colors.purple[900], fontFamily: "OpenSans"),
+                    color: Colors.purple[900], fontFamily: "Helvetica"),
                 prefixIcon: Icon(
-                  Icons.account_circle_outlined,
+                  Icons.lock_outline,
                   color: Colors.purple[900],
                 ),
                 border: OutlineInputBorder(
@@ -102,15 +164,26 @@ class _SignUpStageState extends State<SignUpPage> {
                 fillColor: Colors.white,
                 filled: true)));
     Container confirmpassword = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
         width: MediaQuery.of(context).size.width / 1.2,
         height: MediaQuery.of(context).size.height / 11.5,
         child: TextField(
             decoration: InputDecoration(
                 hintText: "Подтверждение пароля",
                 hintStyle: TextStyle(
-                    color: Colors.purple[900], fontFamily: "OpenSans"),
+                    color: Colors.purple[900], fontFamily: "Helvetica"),
                 prefixIcon: Icon(
-                  Icons.account_circle_outlined,
+                  Icons.contact_mail_outlined,
                   color: Colors.purple[900],
                 ),
                 border: OutlineInputBorder(
@@ -118,15 +191,26 @@ class _SignUpStageState extends State<SignUpPage> {
                 fillColor: Colors.white,
                 filled: true)));
     Container invitecode = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 5,
+              blurRadius: 5,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
         width: MediaQuery.of(context).size.width / 1.2,
         height: MediaQuery.of(context).size.height / 11.5,
         child: TextField(
             decoration: InputDecoration(
                 hintText: "Код приглашения",
                 hintStyle: TextStyle(
-                    color: Colors.purple[900], fontFamily: "OpenSans"),
+                    color: Colors.purple[900], fontFamily: "Helvetica"),
                 prefixIcon: Icon(
-                  Icons.account_circle_outlined,
+                  Icons.quick_contacts_mail_outlined,
                   color: Colors.purple[900],
                 ),
                 border: OutlineInputBorder(
@@ -134,6 +218,17 @@ class _SignUpStageState extends State<SignUpPage> {
                 fillColor: Colors.white,
                 filled: true)));
     Container signUp = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.purple.shade700.withOpacity(0.5),
+              spreadRadius: 0.01,
+              blurRadius: 29,
+              offset: Offset(0, 24.4), // changes position of shadow
+            ),
+          ],
+        ),
         padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 24),
         child: ElevatedButton(
           onPressed: () {},
@@ -144,7 +239,7 @@ class _SignUpStageState extends State<SignUpPage> {
               child: Text(
                 'Зарегистрироваться',
                 style: TextStyle(
-                    color: Colors.white, fontFamily: "OpenSans", fontSize: 18),
+                    color: Colors.white, fontFamily: "Helvetica", fontSize: 18),
               ),
             ),
           ),
@@ -163,14 +258,14 @@ class _SignUpStageState extends State<SignUpPage> {
             context, MaterialPageRoute(builder: (context) => Auth()));
       },
       child: Container(
-        width: MediaQuery.of(context).size.width / 3.7,
+        width: MediaQuery.of(context).size.width / 3.4,
         height: MediaQuery.of(context).size.height / 16,
         child: Center(
           child: Text(
             'Авторизация',
             style: TextStyle(
               color: Colors.purple[700],
-              fontFamily: "OpenSans",
+              fontFamily: "Helvetica",
               fontSize: 18,
             ),
           ),
@@ -186,13 +281,24 @@ class _SignUpStageState extends State<SignUpPage> {
     ElevatedButton regbtn = ElevatedButton(
       onPressed: () {},
       child: Container(
-        width: MediaQuery.of(context).size.width / 3.7,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.purple.shade700.withOpacity(0.6),
+              spreadRadius: 5,
+              blurRadius: 10,
+              offset: Offset(0, 5), // changes position of shadow
+            ),
+          ],
+        ),
+        width: MediaQuery.of(context).size.width / 3.4,
         height: MediaQuery.of(context).size.height / 16,
         child: const Center(
           child: Text(
             'Регистрация',
             style: TextStyle(
-                color: Colors.white, fontFamily: "OpenSans", fontSize: 19),
+                color: Colors.white, fontFamily: "Helvetica", fontSize: 19),
           ),
         ),
       ),
@@ -215,17 +321,28 @@ class _SignUpStageState extends State<SignUpPage> {
         home: Scaffold(
             resizeToAvoidBottomInset: false,
             backgroundColor: Colors.purple[100],
-            body: Column(children: <Widget>[
-              registr,
-              surname,
-              name,
-              email,
-              phone,
-              password,
-              invitecode,
-              signUp,
-              Spacer(),
-              authreg
-            ])));
+            body: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.white,
+                    Colors.purple.shade200,
+                    Colors.yellow.shade300,
+                  ],
+                )),
+                child: Column(children: <Widget>[
+                  registr,
+                  surname,
+                  name,
+                  email,
+                  phone,
+                  password,
+                  invitecode,
+                  signUp,
+                  Spacer(),
+                  authreg
+                ]))));
   }
 }
--- lib/auth.dart
+++ lib/auth.dart
@@ -16,12 +16,30 @@ class Auth extends StatelessWidget {
     Text auth = Text("Авторизация",
         textDirection: TextDirection.ltr,
         style: TextStyle(
-            fontSize: 35,
-            color: Colors.purple[800],
-            fontWeight: FontWeight.bold,
-            fontFamily: "OpenSans"));
+          fontSize: 35,
+          color: Colors.purple[800],
+          fontWeight: FontWeight.bold,
+          fontFamily: "Helvetica",
+          shadows: const <Shadow>[
+            Shadow(
+                offset: Offset(1.0, 1.0),
+                blurRadius: 5.0,
+                color: Colors.purple),
+          ],
+        ));
 
     Container login = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
         width: MediaQuery.of(context).size.width / 1.2,
         height: MediaQuery.of(context).size.height / 11.5,
         // padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 50),
@@ -29,7 +47,7 @@ class Auth extends StatelessWidget {
             decoration: InputDecoration(
                 hintText: "Эл. почта или телефон",
                 hintStyle: TextStyle(
-                    color: Colors.purple[900], fontFamily: "OpenSans"),
+                    color: Colors.purple[900], fontFamily: "Helvetica"),
                 prefixIcon: Icon(
                   Icons.account_circle_outlined,
                   color: Colors.purple[900],
@@ -40,6 +58,17 @@ class Auth extends StatelessWidget {
                 filled: true)));
 
     Container password = Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
         width: MediaQuery.of(context).size.width / 1.2,
         height: MediaQuery.of(context).size.height / 11.5,
         // padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 50),
@@ -51,7 +80,7 @@ class Auth extends StatelessWidget {
                   color: Colors.purple[900],
                 ),
                 hintStyle: TextStyle(
-                    color: Colors.purple[900], fontFamily: "OpenSans"),
+                    color: Colors.purple[900], fontFamily: "Helvetica"),
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(35.0)),
                 fillColor: Colors.white,
@@ -65,13 +94,24 @@ class Auth extends StatelessWidget {
                 context, MaterialPageRoute(builder: (context) => Chats()));
           },
           child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(35.0)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.purple.shade700.withOpacity(0.6),
+                  spreadRadius: 8,
+                  blurRadius: 30,
+                  offset: Offset(0, 10), // changes position of shadow
+                ),
+              ],
+            ),
             width: MediaQuery.of(context).size.width / 4,
             height: MediaQuery.of(context).size.height / 16,
             child: Center(
               child: Text(
                 'Войти',
                 style: TextStyle(
-                    color: Colors.white, fontFamily: "OpenSans", fontSize: 18),
+                    color: Colors.white, fontFamily: "Helvetica", fontSize: 18),
               ),
             ),
           ),
@@ -88,13 +128,24 @@ class Auth extends StatelessWidget {
     ElevatedButton authbtn = ElevatedButton(
       onPressed: () {},
       child: Container(
-        width: MediaQuery.of(context).size.width / 3.7,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.purple.shade700.withOpacity(0.6),
+              spreadRadius: 5,
+              blurRadius: 10,
+              offset: Offset(0, 5), // changes position of shadow
+            ),
+          ],
+        ),
+        width: MediaQuery.of(context).size.width / 3.4,
         height: MediaQuery.of(context).size.height / 16,
         child: Center(
           child: Text(
             'Авторизация',
             style: TextStyle(
-                color: Colors.white, fontFamily: "OpenSans", fontSize: 18),
+                color: Colors.white, fontFamily: "Helvetica", fontSize: 18),
           ),
         ),
       ),
@@ -116,14 +167,14 @@ class Auth extends StatelessWidget {
             context, MaterialPageRoute(builder: (context) => SignUpPage()));
       },
       child: Container(
-        width: MediaQuery.of(context).size.width / 3.7,
+        width: MediaQuery.of(context).size.width / 3.4,
         height: MediaQuery.of(context).size.height / 16,
         child: Center(
           child: Text(
             'Регистрация',
             style: TextStyle(
                 color: Colors.purple[800],
-                fontFamily: "OpenSans",
+                fontFamily: "Helvetica",
                 fontSize: 19),
           ),
         ),
@@ -148,16 +199,27 @@ class Auth extends StatelessWidget {
         home: Scaffold(
             resizeToAvoidBottomInset: false,
             backgroundColor: Colors.purple[100],
-            body: Column(children: <Widget>[
-              Spacer(),
-              auth,
-              Spacer(),
-              login,
-              password,
-              signIn,
-              Spacer(),
-              inup,
-            ] //child: test,
-                )));
+            body: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.white,
+                    Colors.purple.shade200,
+                    Colors.yellow.shade300,
+                  ],
+                )),
+                child: Column(children: <Widget>[
+                  Spacer(),
+                  auth,
+                  Spacer(),
+                  login,
+                  password,
+                  signIn,
+                  Spacer(),
+                  inup,
+                ] //child: test,
+                    ))));
   }
 }
